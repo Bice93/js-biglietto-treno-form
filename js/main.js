65e718192819a962535ed5c1d57e61b6c2574beb
Bice93 <bianca.cerulo@hotmail.it>
2022-06-16 09:28:02 +0200

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -7,28 +7,52 @@
 // va applicato uno sconto del 35% per gli over 65.
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+// MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 
 
+const nameUser = document.querySelector(`#name-user`);
 const kmTravel = document.querySelector(`#km-travel`);
 const userAge = document.querySelector(`#age-user`);
 const priceBox = document.querySelector(`#price`);
-
+const nameUserSee = document.querySelector(`#name_user_see`);
 const sendButton = document.querySelector(`#btn-send`);
-sendButton.addEventListener(`click`, function(){
+const offerUser = document.querySelector(`#offer_user`);
+const numberCab = document.querySelector(`#number_cab`);
+const codeUser = document.querySelector(`#code_user`);
+
+sendButton.addEventListener(`click`, function () {
+    console.log(document.querySelector(`#name-user`).value);
     console.log(document.querySelector(`#km-travel`).value);
     console.log(document.querySelector(`#age-user`).value);
-    let priceTicket = kmTravel.value * 0.26;
-     if (userAge.value < 18) {
-        priceTicket = priceTicket - ((priceTicket * 15) / 100);
-        console.log(`il passeggero è minorenne, il prezzo del biglietto è: € ${priceTicket.toFixed(2)}`);
-        priceBox.innerHTML += priceTicket.toFixed(2);
-    } else if (userAge.value > 65) {
-        priceTicket = priceTicket - ((priceTicket * 35) / 100);
-        console.log(`il passeggero è un over 65, il prezzo del biglietto è: € ${priceTicket.toFixed(2)}`);
-        priceBox.innerHTML += priceTicket.toFixed(2);
+
+    if ((isNaN(kmTravel.value) == true) || (isNaN(userAge.value) == true)) {
+        console.log(`Numero non accettato. Inserisci correttamente i tuoi dati`);
+        alert(`Inserisci correttamente i tuoi dati`);
     } else {
-        console.log(`Il prezzo del biglietto è: € ${priceTicket.toFixed(2)}`);
-        priceBox.innerHTML += priceTicket.toFixed(2);
+        let priceTicket = kmTravel.value * 0.26;
+
+        if (userAge.value < 18) {
+            priceTicket = priceTicket - ((priceTicket * 15) / 100);
+            console.log(`Il passeggero è minorenne, il prezzo del biglietto è: € ${priceTicket.toFixed(2)}`);
+            priceBox.innerHTML = "€" + " " + priceTicket.toFixed(2);
+            offerUser.innerHTML = "Offerta Giovane";
+        } else if (userAge.value > 65) {
+            priceTicket = priceTicket - ((priceTicket * 35) / 100);
+            console.log(`Il passeggero è un over 65, il prezzo del biglietto è: € ${priceTicket.toFixed(2)}`);
+            priceBox.innerHTML = "€" + " " + priceTicket.toFixed(2);
+            offerUser.innerHTML = "Offerta Senior";
+        } else {
+            console.log(`Il prezzo del biglietto è: € ${priceTicket.toFixed(2)}`);
+            priceBox.innerHTML = "€" + " " + priceTicket.toFixed(2);
+            offerUser.innerHTML = "Biglietto Standard";
+        }
+
+        nameUserSee.innerHTML = nameUser.value;
+        let randomNumber = Math.floor((Math.random() * 12) + 1);
+        numberCab.innerHTML = randomNumber;
+        let randomNumberCode = Math.floor((Math.random() * 12000) + 1);
+        codeUser.innerHTML = randomNumberCode;
     }
 });
 
